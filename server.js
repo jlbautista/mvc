2490552075771237dbc752e02ec2b3b497554cda
JL Bautista <jl.bautistac@gmail.com>
2025-03-03 16:36:06 -0600

    Add sync method at connect to db

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app');
 const PORT = process.env.PORT || 3001;
+const app = require('./app');
+const db = require('./config/db');
+
+// Connect to DB
+db.sync()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database: ', err);
+    });
 
 // Start the server only if this file is run directly
 if (require.main === module) {
