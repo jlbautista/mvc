2bfea3fc868f65349a0a5c1d4dc683267f1b5c0e
JL Bautista <jl.bautistac@gmail.com>
2025-03-03 16:44:37 -0600

    Add purchase routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const usersRoutes = require("./routes/usersRoutes");
 const cors = require("cors"); // CSRF
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+const usersRoutes = require("./routes/usersRoutes");
+const purchasesRoutes = require("./routes/purchasesRoutes");
+
 app.use("/api/users", usersRoutes);
+app.use("/api/purchases", purchasesRoutes);
 
 // Error handler middleware
 app.use((err, req, res, next) => {
