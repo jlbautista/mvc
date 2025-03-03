a58c30c7be792258dd263b6a5378acb29387b147
JL Bautista <jl.bautistac@gmail.com>
2025-03-03 16:36:54 -0600

    Add Purchase Model

---
+++ models/purchase.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const Product = require('./product');
+
+const Purchase = sequelize.define('Purchase', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    total: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    productId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }, 
+        allowNull: false
+    }
+});
+
+module.exports = Purchase;
