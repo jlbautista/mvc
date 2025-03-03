c8cc6e73be7f9e88241431eafb2f2fbb552d0435
JL Bautista <jl.bautistac@gmail.com>
2025-03-03 16:46:35 -0600

    Update Purchases Controller reference in routes

--- routes/purchaseRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const purchaseController = require('../controllers/purchaseController');
-
-router.post('/buy', purchaseController.buyProduct);
-
-module.exports = router;
