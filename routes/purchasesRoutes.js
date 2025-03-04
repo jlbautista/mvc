0034ee82596e6bbf9ee2af80e6468da9446fa3c3
JL Bautista <jl.bautistac@gmail.com>
2025-03-04 13:43:09 -0600

    Add swagger in Purchases Routes

--- routes/purchasesRoutes.js
+++ routes/purchasesRoutes.js
@@ -2,6 +2,32 @@ const express = require('express');
 const router = express.Router();
 const purchasesController = require('../controllers/purchasesController');
 
+/**
+ * @swagger
+ * /purchases:
+ *  post:
+ *    tags: [Purchases]
+ *    summary: Buy a product
+ *    description : Buy a product
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              productId:
+ *                type: string
+ *              quantity:
+ *                type: number
+ *    responses:
+ *      '200':
+ *        description: Product bought
+ *      '400':
+ *        description: Bad request
+ *      '500':
+ *        description: Internal server error
+ */
 router.post('/', purchasesController.buyProduct);
 
 module.exports = router;
