6e6e4c841a28ba0cd715b5afb834e86152b33369
JL Bautista <jl.bautistac@gmail.com>
2025-03-18 08:39:44 -0600

    Add products swagger documentation

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -2,10 +2,167 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 price:
+ *                   type: number
+ *     responses:
+ *       201:
+ *         description: The product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   price:
+ *                     type: number
+ *       500:
+ *         description: Some server error
+ */
 router.post('/', productsController.create);
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     requestBody:
+ *       required: false
+ *     responses:
+ *       200:
+ *         description: The list of products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   name:
+ *                     type: string
+ *                   price:
+ *                     type: number
+ *       500:
+ *         description: Some server error
+ */
 router.get('/', productsController.getProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 price:
+ *                   type: number
+ *       404:
+ *         description: The product was not found
+ *       500:
+ *         description: Some server error
+ */
 router.get('/:id', productsController.getProduct);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Update a product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: The product was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 price:
+ *                   type: number
+ *       404:
+ *         description: The product was not found
+ *       500:
+ *         description: Some server error
+ */
 router.put('/:id', productsController.update);
+
+/** 
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Remove a product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: The product was deleted
+ *       404:
+ *         description: The product was not found
+ *       500:
+ *        description: Some server error
+ */
 router.delete('/:id', productsController.delete);
 
 module.exports = router;
